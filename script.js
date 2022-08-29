43234944b66c63a5b735a6b55e59163ce3c725a9
Joao Bosco <jboscos.carvalho@gmail.com>
2022-08-29 13:50:54 -0300

    js desenvolvido

---
+++ script.js
@@ -0,0 +1,69 @@
+const nomeUsuario = prompt('Qual seu nome?');
+console.log(nomeUsuario);
+
+let dados;
+
+function atualiza(){
+  const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+  promessa.then(processarResposta);
+  promessa.catch(tratarErro);
+}
+
+setInterval(atualiza, 3000);
+
+function processarResposta(resposta) {
+  console.log(resposta);
+	console.log(resposta.data);
+
+  dados = resposta.data;
+
+  renderizarMensagem();
+}
+
+function tratarErro() {
+  window.location.reload();
+}
+
+//const requisicao = axios.post('http://...', dados);
+
+//requisicao.then(tratarSucesso);
+//requisicao.catch(tratarError);
+
+function renderizarMensagem(){
+  const conteudo = document.querySelector('.content');
+  conteudo.innerHTML = '';
+
+  for(let i = 0; i < dados.length; i++){
+    if(dados[i].type === "status"){
+      conteudo.innerHTML = conteudo.innerHTML + `
+      <div class="msg ${dados[i].type}">
+        <p><span>(${dados[i].time}) </span><strong>${dados[i].from}</strong> ${dados[i].text}</p>
+      </div>
+    `;
+    }
+
+    else{
+      conteudo.innerHTML = conteudo.innerHTML + `
+      <div class="msg ${dados[i].type}">
+        <p><span>(${dados[i].time}) </span><strong>${dados[i].from}</strong> para <strong>${dados[i].to}</strong>: ${dados[i].text}</p>
+      </div>
+    `;
+    }
+  }
+}
+
+function mandarMensagem(){
+  const minhaMensagem = document.getElementById("input").value;
+  document.getElementById("input").value = '';
+  const  novaMensagem = {
+    from: nomeUsuario,
+    to: "Todos",
+    text: minhaMensagem,
+    type: "message"
+  }
+
+  const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem); 
+  requisicao.then(renderizarMensagem);
+  requisicao.catch(tratarError);
+}
